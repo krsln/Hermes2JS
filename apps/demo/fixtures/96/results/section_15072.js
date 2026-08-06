e4589d3c530ab9215691ccc6a8edaf91028b8e99
krsln <krsln@users.noreply.github.com>
2026-08-06 23:57:02 +0300

    2026-08-06

--- apps/demo/fixtures/96/results/section_15072.js
+++ apps/demo/fixtures/96/results/section_15072.js
@@ -25,32 +25,14 @@ function logicalShortCircuitTest(param0, param1, param2) {
     // CODE → <Call3>: <Reg8: 4, Reg8: 7, Reg8: 0, Reg8: 4, Reg8: 3>
     r4 = sideEffect.call(undefined, "and-left", param1)
     // CODE → <Mov>: <Reg8: 6, Reg8: 3>
-    r6 = param1
-    if (param1) {
-        // ──────────────── Block 1 ──────────────── 
-        // CODE → <LoadConstString>: <Reg8: 4, string_id: 5117>  # String: 'and-right' (String)
-        // USED → r4 = "and-right";
-        // CODE → <Call3>: <Reg8: 4, Reg8: 7, Reg8: 0, Reg8: 4, Reg8: 2>
-        r4 = sideEffect.call(undefined, "and-right", param2)
-        // CODE → <Mov>: <Reg8: 6, Reg8: 2>
-        // USED → r6 = param2;
-    }
+    r6 = param1 && param2
     // ──────────────── Block 2 ──────────────── 
     // CODE → <LoadConstString>: <Reg8: 4, string_id: 1184>  # String: 'or-left' (String)
     // USED → r4 = "or-left";
     // CODE → <Call3>: <Reg8: 4, Reg8: 7, Reg8: 0, Reg8: 4, Reg8: 3>
     r4 = sideEffect.call(undefined, "or-left", param1)
     // CODE → <Mov>: <Reg8: 5, Reg8: 3>
-    // USED → r5 = param1;
-    if (!param1) {
-        // ──────────────── Block 3 ──────────────── 
-        // CODE → <LoadConstString>: <Reg8: 4, string_id: 1951>  # String: 'or-right' (String)
-        // USED → r4 = "or-right";
-        // CODE → <Call3>: <Reg8: 4, Reg8: 7, Reg8: 0, Reg8: 4, Reg8: 2>
-        r4 = sideEffect.call(undefined, "or-right", param2)
-        // CODE → <Mov>: <Reg8: 5, Reg8: 2>
-        // USED → r5 = param2;
-    }
+    // USED → r5 = param1 || param2;
     // ──────────────── Block 4 ──────────────── 
     // CODE → <LoadConstString>: <Reg8: 4, string_id: 74>  # String: 'left' (Identifier)
     r4 = "left"
@@ -77,7 +59,7 @@ function logicalShortCircuitTest(param0, param1, param2) {
     // CODE → <GetByIdShort>: <Reg8: 2, Reg8: 3, UInt8: 2, string_id: 90>  # String: 'log' (Identifier)
     // USED → r2 = globalThis.console.log;
     // CODE → <Call4>: <Reg8: 2, Reg8: 2, Reg8: 3, Reg8: 6, Reg8: 5, Reg8: 4>
-    r2 = globalThis.console.log(param2, param2, param2)
+    r2 = globalThis.console.log(param1 && param2, param1 || param2, param2)
     // CODE → <TryGetById>: <Reg8: 3, Reg8: 1, UInt8: 1, string_id: 99>  # String: 'console' (Identifier)
     // USED → r3 = globalThis.console;
     // CODE → <GetByIdShort>: <Reg8: 2, Reg8: 3, UInt8: 2, string_id: 90>  # String: 'log' (Identifier)
