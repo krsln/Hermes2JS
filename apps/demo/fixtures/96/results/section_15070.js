8a824af3d29fae72aa608ad30fb35a16b369cf5f
krsln <krsln@users.noreply.github.com>
2026-08-07 09:32:08 +0300

    2026-08-07

--- apps/demo/fixtures/96/results/section_15070.js
+++ apps/demo/fixtures/96/results/section_15070.js
@@ -17,20 +17,7 @@ function ternaryTest(param0, param1) {
     // CODE → <Greater>: <Reg8: 3, Reg8: 2, Reg8: 1>
     // USED → r3 = param1 > 0;
     // CODE → <LoadConstString>: <Reg8: 5, string_id: 866>  # String: 'positive' (String)
-    r5 = "positive"
-    if (param1 <= 0 && param1 < 0) {
-        // ──────────────── Block 1 ──────────────── 
-        // CODE → <Less>: <Reg8: 4, Reg8: 2, Reg8: 1>
-        // USED → r4 = param1 < 0;
-        // CODE → <LoadConstString>: <Reg8: 3, string_id: 615>  # String: 'zero' (String)
-        r3 = "zero"
-        // ──────────────── Block 2 ──────────────── 
-        // CODE → <LoadConstString>: <Reg8: 3, string_id: 1323>  # String: 'negative' (String)
-        // USED → r3 = "negative";
-        // ──────────────── Block 3 ──────────────── 
-        // CODE → <Mov>: <Reg8: 5, Reg8: 3>
-        // USED → r5 = "negative";
-    }
+    r5 = !(param1 <= 0 && param1 < 0) ? "positive" : "negative"
     // ──────────────── Block 4 ──────────────── 
     // CODE → <TryGetById>: <Reg8: 4, Reg8: 0, UInt8: 1, string_id: 99>  # String: 'console' (Identifier)
     // USED → r4 = globalThis.console;
