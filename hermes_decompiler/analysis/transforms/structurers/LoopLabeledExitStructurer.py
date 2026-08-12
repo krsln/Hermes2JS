b9d24741e5c23aa23e55943b813d43f7fb9558f2
krsln <krsln@users.noreply.github.com>
2026-08-12 14:37:28 +0300

    2026-08-12

--- hermes_decompiler/analysis/transforms/structurers/LoopLabeledExitStructurer.py
+++ hermes_decompiler/analysis/transforms/structurers/LoopLabeledExitStructurer.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 from hermes_decompiler.analysis.cfg import BasicBlock
-from hermes_decompiler.analysis.terminators import TerminatorConditionalBranch, TerminatorJump
 from hermes_decompiler.analysis.regions.Regions import SequenceRegion, LoopRegion, IfRegion
+from hermes_decompiler.analysis.terminators import TerminatorConditionalBranch, TerminatorJump
 from hermes_decompiler.analysis.transforms._shared._negation import _negate_condition
 from hermes_decompiler.analysis.transforms.structurers._base import RegionStructurer
 from hermes_decompiler.core.logging import get_logger
