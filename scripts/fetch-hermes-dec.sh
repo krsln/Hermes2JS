806cb2559f16bb68f0c40b39fed5cb90ca1237f7
krsln <krsln@users.noreply.github.com>
2026-08-14 14:33:12 +0300

    2026-08-14

--- scripts/fetch-hermes-dec.sh
+++ scripts/fetch-hermes-dec.sh
@@ -3,7 +3,7 @@ set -e
 
 # git ls-remote https://github.com/P1sec/hermes-dec.git HEAD
 #HERMES_DEC_COMMIT="66bb3449ace7ed48b400878da045e89e3a45bff2"
-HERMES_DEC_COMMIT="648be12f721775d952d6e67b20b5930c929b2495"
+HERMES_DEC_COMMIT="a0f18f97ab661eb8ed659c8c683a0d21ea619e69"
 VENDOR_DIR="vendor/hermes-dec"
 
 if [[ ! -d "$VENDOR_DIR" ]]; then
