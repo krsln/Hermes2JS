8db976cb33565e05ae5ee8193e3e33ec24ec34cd
krsln <krsln@users.noreply.github.com>
2026-09-06 22:27:35 +0300

    2026-09-06

--- hermes_decompiler/ir/statements/__init__.py
+++ hermes_decompiler/ir/statements/__init__.py
@@ -7,3 +7,15 @@ from .ControlFlow import *
 from .Loops import *
 from .Jumps import *
 from .ExceptionHandling import *
+
+__all__ = [
+    "Statement",
+
+    "BlockStatement", "EmptyStatement",
+    "ExpressionStatement",
+    "VariableDeclarator", "VariableDeclaration", "FunctionDeclaration", "ClassDeclaration",
+    "IfStatement", "ReturnStatement", "DebuggerStatement",
+    "WhileStatement", "DoWhileStatement", "ForStatement", "ForInStatement", "ForOfStatement",
+    "LabeledStatement", "BreakStatement", "ContinueStatement",
+    "ThrowStatement", "CatchClause", "FinallyClause", "TryStatement",
+]
